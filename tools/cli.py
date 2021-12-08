aa9848fa969df614a77eec7ca67aa364fcbd43dd
shenmishajing <shenmishajing@gmail.com>
2021-12-08 15:10:35 +0800

    fix RuntimeError: received 0 items of ancdata bug

--- tools/cli.py
+++ tools/cli.py
@@ -1,6 +1,9 @@
+import torch
 from pytorch_lightning.utilities.cli import LightningCLI, DATAMODULE_REGISTRY
 
 from utils.callbacks.save_and_log_config_callback import SaveAndLogConfigCallback
+
+torch.multiprocessing.set_sharing_strategy('file_system')
 
 DATAMODULE_REGISTRY(object)
 

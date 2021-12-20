2818203d3c74559464ed03fc7c9ca077236eff44
shenmishajing <shenmishajing@gmail.com>
2021-12-20 16:16:42 +0800

    add show version param in RichDefaultThemeProgressBar callback feature

--- utils/progress/rich_progress.py
+++ utils/progress/rich_progress.py
@@ -1,4 +1,4 @@
-from typing import Dict, Union
+from typing import Dict, Union, Optional
 
 import pytorch_lightning as pl
 from pytorch_lightning.callbacks.progress.rich_progress import RichProgressBar, RichProgressBarTheme
@@ -8,13 +8,16 @@ class RichDefaultThemeProgressBar(RichProgressBar):
 
     def __init__(
             self,
-            refresh_rate_per_second: int = 10,
-            leave: bool = False
+            refresh_rate_per_second: Optional[int] = 10,
+            leave: Optional[bool] = False,
+            show_version: Optional[bool] = True,
     ) -> None:
         super().__init__(refresh_rate_per_second = refresh_rate_per_second, leave = leave, theme = RichProgressBarTheme())
+        self.show_version = show_version
 
     def get_metrics(self, trainer: "pl.Trainer", pl_module: "pl.LightningModule") -> Dict[str, Union[int, str]]:
-        # don't show the version number
         items = super().get_metrics(trainer, pl_module)
-        items.pop("v_num", None)
+        if not self.show_version:
+            # don't show the version number
+            items.pop("v_num", None)
         return items

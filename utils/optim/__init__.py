b0a13995f65c3de21b5135a44c8f42a7c058e8cf
shenmishajing <shenmishajing@gmail.com>
2021-12-20 16:15:59 +0800

    add warm up scheduler feature

---
+++ utils/optim/__init__.py
@@ -0,0 +1,4 @@
+from torch.optim import *
+from torch.optim.lr_scheduler import *
+
+from .warmup_lr_scheduler import WarmupScheduler
---
+++ utils/optim/warmup_lr_scheduler.py
@@ -0,0 +1,25 @@
+import warnings
+from torch.optim.lr_scheduler import _LRScheduler
+
+
+class WarmupScheduler(_LRScheduler):
+    """ Warm-up(increasing) learning rate in optimizer.
+
+    Args:
+        optimizer (Optimizer): Wrapped optimizer.
+        warmup_iters: target warm up epoch.
+    """
+
+    def __init__(self, optimizer, warmup_iters, last_epoch = -1, verbose = False):
+        self.warmup_iters = warmup_iters
+        super().__init__(optimizer, last_epoch, verbose)
+
+    def get_lr(self):
+        if not self._get_lr_called_within_step:
+            warnings.warn("To get the last learning rate computed by the scheduler, "
+                          "please use `get_last_lr()`.", UserWarning)
+
+        if self.last_epoch <= self.warmup_iters:
+            return [base_lr * self.last_epoch / self.warmup_iters
+                    for base_lr in self.base_lrs]
+        return [group['lr'] for group in self.optimizer.param_groups]

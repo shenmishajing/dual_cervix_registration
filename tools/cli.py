0c5fda3dadf41f31c4040c47e4108ca8cd2440dc
shenmishajing <shenmishajing@gmail.com>
2021-12-20 16:18:11 +0800

    remove custom CLI class in cli.py move torch and datamodule settings into main func

--- tools/cli.py
+++ tools/cli.py
@@ -3,17 +3,12 @@ from pytorch_lightning.utilities.cli import LightningCLI, DATAMODULE_REGISTRY
 
 from utils.callbacks.save_and_log_config_callback import SaveAndLogConfigCallback
 
-torch.multiprocessing.set_sharing_strategy('file_system')
-
-DATAMODULE_REGISTRY(object)
-
-
-class CLI(LightningCLI):
-    pass
-
 
 def main():
-    CLI(save_config_callback = SaveAndLogConfigCallback)
+    torch.multiprocessing.set_sharing_strategy('file_system')
+    DATAMODULE_REGISTRY(object)
+
+    LightningCLI(save_config_callback = SaveAndLogConfigCallback)
 
 
 if __name__ == '__main__':

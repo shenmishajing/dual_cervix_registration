f46c577427a4446349e4e6ee2fdb43fa6a138ce0
shenmishajing <shenmishajing@gmail.com>
2021-12-24 10:18:06 +0800

    add parse string feature to config files

--- utils/cli.py
+++ utils/cli.py
@@ -28,7 +28,25 @@ def deep_update(source, overrides):
         return overrides
 
 
-def parse_config(parser, cfg_path, seen_cfg = None, **kwargs):
+def parse_config(parser, cfg_file, cfg_path = None, **kwargs):
+    if '__base__' in cfg_file:
+        sub_cfg_paths = cfg_file.pop('__base__')
+        if sub_cfg_paths is not None:
+            if not isinstance(sub_cfg_paths, list):
+                sub_cfg_paths = [sub_cfg_paths]
+            sub_cfg_paths = [os.path.join(os.path.dirname(cfg_path), sub_cfg_path) if not sub_cfg_path.startswith(
+                '/') or cfg_path is None else sub_cfg_path for sub_cfg_path in sub_cfg_paths]
+            sub_cfg_file = {}
+            for sub_cfg_path in sub_cfg_paths:
+                sub_cfg_file = deep_update(sub_cfg_file, parse_path(parser, sub_cfg_path, **kwargs).as_dict())
+            cfg_file = parser._apply_actions(deep_update(sub_cfg_file, cfg_file.as_dict()))
+
+    if '__import__' in cfg_file:
+        cfg_file.pop('__import__')
+    return cfg_file
+
+
+def parse_path(parser, cfg_path, seen_cfg = None, **kwargs):
     abs_cfg_path = os.path.abspath(cfg_path)
     if seen_cfg is None:
         seen_cfg = {}
@@ -40,24 +58,13 @@ def parse_config(parser, cfg_path, seen_cfg = None, **kwargs):
 
     cfg_file = parser.parse_path(cfg_path, **kwargs)
     seen_cfg[abs_cfg_path] = None
-
-    if '__base__' in cfg_file:
-        sub_cfg_paths = cfg_file.pop('__base__')
-        if sub_cfg_paths is not None:
-            if not isinstance(sub_cfg_paths, list):
-                sub_cfg_paths = [sub_cfg_paths]
-            sub_cfg_paths = [os.path.join(os.path.dirname(cfg_path), sub_cfg_path) if not sub_cfg_path.startswith('/') else sub_cfg_path for
-                             sub_cfg_path in sub_cfg_paths]
-            sub_cfg_file = {}
-            for sub_cfg_path in sub_cfg_paths:
-                sub_cfg_file = deep_update(sub_cfg_file, parse_config(parser, sub_cfg_path, seen_cfg = seen_cfg, **kwargs).as_dict())
-            cfg_file = parser._apply_actions(deep_update(sub_cfg_file, cfg_file.as_dict()))
-
-    if '__import__' in cfg_file:
-        cfg_file.pop('__import__')
-
+    cfg_file = parse_config(parser, cfg_file, cfg_path = cfg_path, seen_cfg = seen_cfg, **kwargs)
     seen_cfg[abs_cfg_path] = cfg_file
     return cfg_file
+
+
+def parse_string(parser, cfg_string, **kwargs):
+    return parse_config(parser, parser.parse_string(cfg_string, **kwargs), **kwargs)
 
 
 class ActionConfigFile(_ActionConfigFile):
@@ -74,11 +81,11 @@ class ActionConfigFile(_ActionConfigFile):
                     if isinstance(load_value(value), str):
                         raise ex_path
                     cfg_path = None
-                    cfg_file = parser.parse_string(value, **kwargs)
+                    cfg_file = parse_string(parser, value, **kwargs)
                 except (TypeError,) + get_loader_exceptions() as ex_str:
                     raise TypeError(f'Parser key "{dest}": {ex_str}') from ex_str
             else:
-                cfg_file = parse_config(parser, value, **kwargs)
+                cfg_file = parse_path(parser, value, **kwargs)
             cfg[dest].append(cfg_path)
             cfg.update(cfg_file)
 

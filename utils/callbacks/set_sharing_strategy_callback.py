d488b167328e925e8fdc54e8dd915360c4321082
shenmishajing <shenmishajing@gmail.com>
2021-12-27 14:21:07 +0800

    use from import im set_sharing_strategy_callback.py instead of import torch directly

--- utils/callbacks/set_sharing_strategy_callback.py
+++ utils/callbacks/set_sharing_strategy_callback.py
@@ -1,6 +1,6 @@
-import torch
 from typing import Optional
 
+from torch.multiprocessing import set_sharing_strategy, get_all_sharing_strategies
 from pytorch_lightning import LightningModule, Trainer, Callback
 from pytorch_lightning.utilities import rank_zero_warn
 
@@ -16,9 +16,9 @@ class SetSharingStrategyCallback(Callback):
         self.strategy = strategy
 
     def on_before_accelerator_backend_setup(self, trainer: Trainer, pl_module: LightningModule, stage: Optional[str] = None) -> None:
-        all_strategies = torch.multiprocessing.get_all_sharing_strategies()
+        all_strategies = get_all_sharing_strategies()
         if self.strategy in all_strategies:
-            torch.multiprocessing.set_sharing_strategy(self.strategy)
+            set_sharing_strategy(self.strategy)
         else:
             rank_zero_warn(f'Sharing strategy {self.strategy} is not supported.'
                            f'All supported strategy are: {all_strategies}')

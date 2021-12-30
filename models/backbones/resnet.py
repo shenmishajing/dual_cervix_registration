7fa44dc85a9d97f23b71c4ca14cf7e04a875ff63
shenmishajing <shenmishajing@gmail.com>
2021-12-30 11:11:01 +0800

    fix self.inplanes != 64 bug in resnet.py

--- models/backbones/resnet.py
+++ models/backbones/resnet.py
@@ -5,5 +5,4 @@ from mmcv.cnn import ResNet as MMCV_ResNet
 class ResNet(MMCV_ResNet):
     def __init__(self, inplanes, *args, **kwargs):
         super().__init__(*args, **kwargs)
-        self.conv1 = nn.Conv2d(
-            inplanes, self.inplanes, kernel_size = 7, stride = 2, padding = 3, bias = False)
+        self.conv1 = nn.Conv2d(inplanes, 64, kernel_size = 7, stride = 2, padding = 3, bias = False)

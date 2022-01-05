db3bb0724bb4386c1ca04d474fbfe10430f14d35
shenmishajing <shenmishajing@gmail.com>
2022-01-05 11:02:22 +0800

    fix multi gpu bug and symlink do not delete bug in model_checkpoint.py

--- utils/callbacks/model_checkpoint.py
+++ utils/callbacks/model_checkpoint.py
@@ -1,58 +1,65 @@
 import os
-from typing import Dict
-import torch
+from typing import Dict, Optional
 
+from weakref import proxy
 import pytorch_lightning as pl
-from pytorch_lightning.utilities import rank_zero_info
 from pytorch_lightning.utilities.types import _METRIC
-from pytorch_lightning.callbacks import ModelCheckpoint
+from pytorch_lightning.callbacks import ModelCheckpoint as _ModelCheckpoint
 
 
-class ModelCheckpointWithSymlink(ModelCheckpoint):
+class ModelCheckpoint(_ModelCheckpoint):
     CHECKPOINT_NAME_BEST = "best"
 
-    def _update_best_and_save(
-            self, current: torch.Tensor, trainer: "pl.Trainer", monitor_candidates: Dict[str, _METRIC]
-    ) -> None:
-        k = len(self.best_k_models) + 1 if self.save_top_k == -1 else self.save_top_k
+    def __init__(
+            self,
+            save_best: Optional[bool] = None,
+            *args, **kwargs
+    ):
+        super().__init__(*args, **kwargs)
+        self.save_best = save_best
 
-        del_filepath = None
-        if len(self.best_k_models) == k and k > 0:
-            del_filepath = self.kth_best_model_path
-            self.best_k_models.pop(del_filepath)
+    def save_checkpoint(self, trainer: "pl.Trainer") -> None:
+        """Performs the main logic around saving a checkpoint.
 
-        # do not save nan, replace with +/- inf
-        if isinstance(current, torch.Tensor) and torch.isnan(current):
-            current = torch.tensor(float("inf" if self.mode == "min" else "-inf"), device = current.device)
+        This method runs on all ranks. It is the responsibility of `trainer.save_checkpoint` to correctly handle the
+        behaviour in distributed training, i.e., saving only on rank 0 for data parallel use cases.
+        """
+        epoch = trainer.current_epoch
+        global_step = trainer.global_step
 
-        filepath = self._get_metric_interpolated_filepath_name(monitor_candidates, trainer, del_filepath)
+        self._validate_monitor_key(trainer)
 
-        # save the current score
-        self.current_score = current
-        self.best_k_models[filepath] = current
+        # track epoch when ckpt was last checked
+        self._last_global_step_saved = global_step
 
-        if len(self.best_k_models) == k:
-            # monitor dict has reached k elements
-            _op = max if self.mode == "min" else min
-            self.kth_best_model_path = _op(self.best_k_models, key = self.best_k_models.get)
-            self.kth_value = self.best_k_models[self.kth_best_model_path]
+        # what can be monitored
+        monitor_candidates = self._monitor_candidates(trainer, epoch = epoch, step = global_step)
 
-        _op = min if self.mode == "min" else max
-        self.best_model_path = _op(self.best_k_models, key = self.best_k_models.get)
-        self.best_model_score = self.best_k_models[self.best_model_path]
+        # callback supports multiple simultaneous modes
+        # here we call each mode sequentially
+        # Mode 1: save the top k checkpoints
+        self._save_top_k_checkpoint(trainer, monitor_candidates)
+        # Mode 2: save monitor=None checkpoints
+        self._save_none_monitor_checkpoint(trainer, monitor_candidates)
+        # Mode 3: save last checkpoints
+        self._save_last_checkpoint(trainer, monitor_candidates)
+        # Mode 4: save best checkpoints
+        self._save_best_checkpoint(trainer, monitor_candidates)
 
-        if self.verbose:
-            epoch = monitor_candidates.get("epoch")
-            step = monitor_candidates.get("step")
-            rank_zero_info(
-                f"Epoch {epoch:d}, global step {step:d}: {self.monitor} reached {current:0.5f}"
-                f' (best {self.best_model_score:0.5f}), saving model to "{filepath}" as top {k}'
-            )
-        trainer.save_checkpoint(filepath, self.save_weights_only)
+        # notify loggers
+        if trainer.is_global_zero and trainer.logger:
+            trainer.logger.after_save_checkpoint(proxy(self))
 
-        if del_filepath is not None and filepath != del_filepath:
-            trainer.training_type_plugin.remove_checkpoint(del_filepath)
+    def _save_best_checkpoint(self, trainer: "pl.Trainer", monitor_candidates: Dict[str, _METRIC]) -> None:
+        if not self.save_best:
+            return
 
-        best_filepath = self.format_checkpoint_name(monitor_candidates, self.CHECKPOINT_NAME_BEST)
-        trainer.training_type_plugin.remove_checkpoint(best_filepath)
-        os.symlink(os.path.basename(self.best_model_path), best_filepath)
+        filepath = self.format_checkpoint_name(monitor_candidates, self.CHECKPOINT_NAME_BEST)
+
+        if trainer.is_global_zero:
+            if self._fs.lexists(filepath):
+                self._fs.rm_file(filepath)
+            if self._fs.protocol == 'file':
+                os.symlink(os.path.basename(self.best_model_path), filepath)
+            else:
+                self._fs.cp_file(self.best_model_path, filepath)

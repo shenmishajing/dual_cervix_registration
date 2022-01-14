87f69f8b2cb8f1683a84451bbd3df5c4b02cb9f0
shenmishajing <shenmishajing@gmail.com>
2022-01-14 10:42:55 +0800

    save config file only when we have a logger

--- utils/callbacks/save_and_log_config_callback.py
+++ utils/callbacks/save_and_log_config_callback.py
@@ -17,14 +17,14 @@ class SaveAndLogConfigCallback(SaveConfigCallback):
         if trainer.logger is not None:
             trainer.logger.log_hyperparams(self.config[self.config['subcommand']])
 
-        log_dir = get_log_dir(trainer)
-        assert log_dir is not None
-        config_path = os.path.join(log_dir, self.config_filename)
-        if (self.overwrite or not os.path.isfile(config_path)) and trainer.is_global_zero:
-            # save only on rank zero to avoid race conditions on DDP.
-            # the `log_dir` needs to be created as we rely on the logger to do it usually
-            # but it hasn't logged anything at this point
-            get_filesystem(log_dir).makedirs(log_dir, exist_ok = True)
-            self.parser.save(
-                self.config, config_path, skip_none = False, overwrite = self.overwrite, multifile = self.multifile
-            )
+            log_dir = get_log_dir(trainer)
+            assert log_dir is not None
+            config_path = os.path.join(log_dir, self.config_filename)
+            if (self.overwrite or not os.path.isfile(config_path)) and trainer.is_global_zero:
+                # save only on rank zero to avoid race conditions on DDP.
+                # the `log_dir` needs to be created as we rely on the logger to do it usually
+                # but it hasn't logged anything at this point
+                get_filesystem(log_dir).makedirs(log_dir, exist_ok = True)
+                self.parser.save(
+                    self.config, config_path, skip_none = False, overwrite = self.overwrite, multifile = self.multifile
+                )

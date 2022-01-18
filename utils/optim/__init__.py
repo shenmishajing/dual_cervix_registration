88b4651387acf96f0bac9682a29c716742a8410c
shenmishajing <shenmishajing@gmail.com>
2022-01-18 12:33:14 +0800

    add keep_and_linearly_decay_lr_scheduler.py

--- utils/optim/__init__.py
+++ utils/optim/__init__.py
@@ -2,3 +2,4 @@ from torch.optim import *
 from torch.optim.lr_scheduler import *
 
 from .warmup_lr_scheduler import WarmupScheduler
+from .keep_and_linearly_decay_lr_scheduler import KeepAndLinearlyDecayLrScheduler
